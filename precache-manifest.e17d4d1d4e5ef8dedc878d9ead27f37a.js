f3c920d567fe16489093d45e784e809822b5017d
Zakhar Shchipets <zshchipets@gmail.com>
2019-06-11 22:10:45 +0300

    Updates

---
+++ precache-manifest.e17d4d1d4e5ef8dedc878d9ead27f37a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3d322ad82b09f36888922988e29371",
+    "url": "/megazord-generator/index.html"
+  },
+  {
+    "revision": "308b9d830a105f97ed76",
+    "url": "/megazord-generator/static/css/main.bf114f38.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/megazord-generator/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "308b9d830a105f97ed76",
+    "url": "/megazord-generator/static/js/main.1c103cf3.chunk.js"
+  },
+  {
+    "revision": "19d693cdaca79dbd1c3b",
+    "url": "/megazord-generator/static/js/runtime~main.9d4bd0fa.js"
+  }
+]);
